db894227c41f976d37044a64a54a1a064170d46d
fjcloud <fj@msl.cloud>
2023-07-27 14:35:13 +0200

    Create main.js

---
+++ html/js/main.js
@@ -0,0 +1,8 @@
+import BlogNavigation from 'js/md-blog.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    new BlogNavigation({
+        zeroMdUrl: 'https://raw.githubusercontent.com/fjcloud/hello/main/index.md',
+        postsPath: '/posts/',
+    });
+});
