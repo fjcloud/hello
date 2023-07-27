f2989a02b3a1044908f04b994d2eb6ba958b0e81
fjcloud <fj@msl.cloud>
2023-07-27 15:14:54 +0200

    Update main.js

--- html/js/main.js
+++ html/js/main.js
@@ -2,7 +2,6 @@ import BlogNavigation from './md-blog.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     new BlogNavigation({
-        zeroMdUrl: 'https://raw.githubusercontent.com/fjcloud/hello/main/index.md',
-        postsPath: '/posts/',
+        zeroMdUrl: 'https://raw.githubusercontent.com/fjcloud/hello/main/index.md'
     });
 });
