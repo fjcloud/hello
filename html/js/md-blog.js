af8664e4a163f4b2d4ad13c3053bf2a6c485239a
Florian Jacquin <fjacquin@redhat.com>
2023-07-28 15:20:31 +0200

    aider: Fix handleRender method to correctly replace the URL when a link in a markdown file is clicked.
    # Aider chat conversation:
    
    USER: voila les logs Handling render for: /posts/helloworld
    md-blog.js:15 Navigating to: /posts/helloworld
    md-blog.js:15 Navigating to: https://raw.githubusercontent.com/fjcloud/hello/main/index.mdposts/helloworld >> this url should be https://raw.githubusercontent.com/fjcloud/hello/main/posts/helloworld.md
    
    ASSISTANT: I see, the issue is that the `navigateTo` method is being called with the wrong URL. This is happening because the `handleRender` method is replacing the URL incorrectly.
    
    To fix this, we need to modify the `handleRender` method to replace the URL correctly.
    
    Here is the change that needs to be made:
    
    ```javascript
    html/js/md-blog.js
    <<<<<<< ORIGINAL
      handleRender() {
        const nodes = this.zeroMdElement.shadowRoot.querySelectorAll('a[href$=".md"]');
        nodes.forEach((node) => {
          node.addEventListener('click', (e) => {
            e.preventDefault();
            const newSrc = node.href.replace(/https:\/\/raw\.githubusercontent\.com\/fjcloud\/hello\/main\/posts\/(.+)\.md/, '/posts/$1');
            console.log(`Handling render for: ${newSrc}`);
            this.navigateTo(newSrc);
            this.backButtonElement.style.display = 'block';
            page(newSrc);
          });
        });
      }
    =======
      handleRender() {
        const nodes = this.zeroMdElement.shadowRoot.querySelectorAll('a[href$=".md"]');
        nodes.forEach((node) => {
          node.addEventListener('click', (e) => {
            e.preventDefault();
            const newSrc = node.href.replace(/https:\/\/raw\.githubusercontent\.com\/fjcloud\/hello\/main\/(.+)\.md/, 'https://raw.githubusercontent.com/fjcloud/hello/main/$1.md');
            console.log(`Handling render for: ${newSrc}`);
            this.navigateTo(newSrc);
            this.backButtonElement.style.display = 'block';
            page(newSrc);
          });
        });
      }
    >>>>>>> UPDATED
    ```
    
    This change will ensure that when a link in a markdown file is clicked, the `handleRender` method correctly replaces the URL, and the `navigateTo` method is called with the correct URL.

--- html/js/md-blog.js
+++ html/js/md-blog.js
@@ -51,7 +51,7 @@ export default class BlogNavigation {
     nodes.forEach((node) => {
       node.addEventListener('click', (e) => {
         e.preventDefault();
-        const newSrc = node.href.replace(/https:\/\/raw\.githubusercontent\.com\/fjcloud\/hello\/main\/posts\/(.+)\.md/, '/posts/$1');
+        const newSrc = node.href.replace(/https:\/\/raw\.githubusercontent\.com\/fjcloud\/hello\/main\/(.+)\.md/, 'https://raw.githubusercontent.com/fjcloud/hello/main/$1.md');
         console.log(`Handling render for: ${newSrc}`);
         this.navigateTo(newSrc);
         this.backButtonElement.style.display = 'block';
